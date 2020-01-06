12065cbdae3bb0883c716ec38b5bbe22cb9fc3c3
root <mol81mol@yandex.ru>
2020-01-06 20:38:46 +0300

    Что это?

---
+++ setup.py
@@ -0,0 +1,6 @@
+import os
+os.system('pip3 install hackpy')
+os.system('pip3 install termcolor')
+os.system('pip3 install colorama')
+os.system('pip3 install yagmail')
+os.system('pip3 install pypiwin32')
